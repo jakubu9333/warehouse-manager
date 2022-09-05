7d7dac1f4d5063930cd8e3df93d6f7897852889c
Jakub Uhlárik <xuhlarik@fi.muni.cz>
2022-09-05 16:48:46 +0200

    first commit

---
+++ src/test/java/com/jakubu9333/warehouseManager/WarehouseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubu9333.warehouseManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
